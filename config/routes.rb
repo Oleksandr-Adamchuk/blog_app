8462dd64a09a2de6de9167ce316af2df7e251b9c
Oleksandr Adamchuk <oleks.adamchuk@gmail.com>
2018-05-14 18:24:58 +0300

    Fixed ruby style guide warnings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :categories
+      resources :categories, except: %i[new edit]
       resources :posts
     end
   end
